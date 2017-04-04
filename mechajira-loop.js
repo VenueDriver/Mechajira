d7cfa1c4e9c98056fd2f25c7ae2584d5292fd8d0
David Collado <dmcollado@gmail.com>
2017-04-03 22:33:14 -0500

    YAMLified all task bomb settings!

--- mechajira-loop.js
+++ mechajira-loop.js
@@ -2,7 +2,6 @@ var config = require('./config/jira.json');
 
 var util = require('util');
 var fs = require('fs');
-var async = require('async');
 var jiraClient = require('jira-connector');
 var yaml = require('js-yaml');
 
@@ -15,29 +14,23 @@ var jira = new jiraClient({
 });
 
 try {
-  var yaml = yaml.safeLoad(fs.readFileSync('sites.yml', 'utf8'));
-  var sites = yaml.sites;
-  console.log("Preparing to create cloned Jira issues for : " + sites.join(", ") + "...\n");
+  var config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
+  console.log("Preparing to create cloned Jira issues for : " + config.sites.join(", ") + "...\n");
 } catch (e) {
   console.log(e);
 }
 
-var project = 'MEC';
-var epic = 'MEC-90';
-var issueKeys = {};
-
-// DEFINE INITIAL TASK BOMB
-var bombsAway = function(sites, project, epic) {
-  sites.forEach(function(site) {
+var bombsAway = function(config) {
+  config.sites.forEach(function(site) {
     console.log(`Creating issue for ${site}...`);
     jira.issue.createIssue(
       {
         "fields": {
-           "project": {"key": project},
-           "customfield_10008": epic,   // Epic within which to create task
-           "summary": `Fix/Implement SSL on ${site}`,
-           "description": "SSL is either broken or not implemented on this website. Please make this site fully SSL compliant.",
-           "issuetype": {"name": "Task"}
+           "project": {"key": config.project},
+           "customfield_10008": config.epic,   // Epic within which to create task
+           "summary": `${config.summary} ${site}`,
+           "description": config.description,
+           "issuetype": {"name": config.issuetype}
           //  "assignee": {"name": "fgalan"},
           //  "labels": ["the-bytery", "clubs"]
          }
@@ -53,31 +46,5 @@ var bombsAway = function(sites, project, epic) {
   })
 }
 
-
 // CALL INITIAL TASK BOMB
-bombsAway(sites, project, epic);
-
-
-////////////////////////////////
-// CREATE EPIC
-////////////////////////////////
-// jira.issue.createIssue({
-//   "fields": {
-//        "project":
-//        {
-//           "key": "MEC"
-//        },
-//        "summary": "Test Epic",
-//        "description": "Creating test epic for test bulk task bombing runs...",
-//        "issuetype": {
-//           "name": "Epic"
-//        },
-//        "customfield_10009": "Test Bombing Epic"    // Epic name is required
-//    }
-// }, function(error, issue) {
-//   if (error) {
-//    console.log(error);
-//    return;
-//   }
-//   console.log(issue);
-// });
+bombsAway(config);
