71f83451e19513351c8e5fd3cdc7963e96b2bce1
Ryan Porter <rap@endymion.com>
2017-10-17 10:25:35 -0400

    Implemented the JIRA Portfolio update command.

--- mechajira.js
+++ mechajira.js
@@ -5,4 +5,5 @@ var chalk = require('chalk');
 program
   .usage('[options] <command>')
   .command('create-list [options] [file]', 'Create a list of JIRA issues from a YAML command file.').alias('cl')
+  .command('portfolio [options] [file]', 'Update JIRA issues from a JIRA Portfolio export CSV file.').alias('p')
   .parse(process.argv);
