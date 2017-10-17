41ca74d5948ac73735359690856747fc8eae7496
Ryan Porter <rap@endymion.com>
2017-10-16 20:06:10 -0400

    Miscellaneous minor tweaks.

--- test/create-list.js
+++ test/create-list.js
@@ -14,37 +14,37 @@ describe('create-list', function() {
       var createIssueStub = sinon.stub(Jira.prototype, "createIssue");
       createList.createList({silent: true}, 'test/files/create-list.yml');
       sinon.assert.callCount(createIssueStub, 4)
-      sinon.assert.calledWith(createIssueStub, sinon.match.any,
+      sinon.assert.calledWith(createIssueStub,
         {
-          project: { key: "MECTEST" },
-          issuetype: { name: "Task" },
+          project: "MECTEST",
+          issuetype: "Task",
           summary: "Do something about alpha",
           description: "This is a task about alpha",
-          customfield_10008: "MECTEST-1"
+          epic: "MECTEST-1"
         })
-      sinon.assert.calledWith(createIssueStub, sinon.match.any,
+      sinon.assert.calledWith(createIssueStub,
         {
-          project: { key: "MECTEST" },
-          issuetype: { name: "Task" },
+          project: "MECTEST",
+          issuetype: "Task",
           summary: "Do something about beta",
           description: "This is a task about beta",
-          customfield_10008: "MECTEST-1"
+          epic: "MECTEST-1"
         })
-      sinon.assert.calledWith(createIssueStub, sinon.match.any,
+      sinon.assert.calledWith(createIssueStub,
         {
-          project: { key: "MECTEST" },
-          issuetype: { name: "Task" },
+          project: "MECTEST",
+          issuetype: "Task",
           summary: "Do something about delta",
           description: "This is a task about delta",
-          customfield_10008: "MECTEST-1"
+          epic: "MECTEST-1"
         })
-      sinon.assert.calledWith(createIssueStub, sinon.match.any,
+      sinon.assert.calledWith(createIssueStub,
         {
-          project: { key: "MECTEST" },
-          issuetype: { name: "Task" },
+          project: "MECTEST",
+          issuetype: "Task",
           summary: "Do something about gamma",
           description: "This is a task about gamma",
-          customfield_10008: "MECTEST-1"
+          epic: "MECTEST-1"
         })
     });
 
