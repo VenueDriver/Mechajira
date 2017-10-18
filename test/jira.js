796623043efea41b8aa00c23065a2f55b113577d
Ryan Porter <rap@endymion.com>
2017-10-17 23:11:09 -0400

    Test coverage for createIssue.

--- test/jira.js
+++ test/jira.js
@@ -7,6 +7,33 @@ const Jira = require('../lib/jira')
 describe('jira', function() {
   describe('#editIssue()', function() {
 
+    it('should make an HTTP call to the createIssue action in the API', async function() {
+      nock('https://yourcompany.atlassian.net', {'encodedQueryParams':true})
+        .post('/rest/api/2/issue', {
+          'fields':
+            {
+              'project':{'key':'PROJECT'},
+              'issuetype':{'name':'Task'},
+              'summary':'SUMMARY',
+              'description':'DESCRIPTION',
+              'customfield_10008':'MECTEST-1'}
+          })
+        .reply(201, {'key':'MECTEST-355'}, [])
+
+      var jira = new Jira(configData({}, 'test/files/config.yml'))
+      var issueData = {
+        'project': 'PROJECT',
+        'issuetype': 'Task',
+        'summary': 'SUMMARY',
+        'description': 'DESCRIPTION',
+        'epic': 'MECTEST-1'
+      }
+      var jiraIssue = await jira.createIssue(issueData)
+      console.log("jiraIssue:")
+      console.dir(jiraIssue)
+      assert.equal('MECTEST-355', jiraIssue.key)
+    })
+
     it('should make an HTTP call to the editIssue action in the API', async function() {
       nock('https://yourcompany.atlassian.net', {'encodedQueryParams':true})
         .put('/rest/api/2/issue/MECTEST-1', {'fields':{'duedate':'2017-10-26'}})
