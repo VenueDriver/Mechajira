61a84b2f2835a76a868d08d8a0beedd4c94beaa2
David Collado <dmcollado@gmail.com>
2017-10-18 11:55:37 -0400

    Trying to figure out how to use jira-coonnector's search() to send a JQL query for all issues with a specific parent

---
+++ delete-children/delete-children.js
@@ -0,0 +1,56 @@
+const config = require('../config/jira.json');
+const util = require('util');
+const jiraClient = require('jira-connector');
+const fs = require('fs')
+
+var jira = new jiraClient({
+  host: config.host,
+  basic_auth: {
+    username: config.username,
+    password: config.password
+  }
+});
+
+var issuekey = "WDEV-4156";
+
+var getIssue = function(issuekey) {
+  console.log(`Retrieving ${issuekey}...`);
+  jira.issue.getIssue({
+    "issueKey": issuekey
+    },
+    function(error, issue) {
+      if (error) {
+        console.log(error);
+        return;
+      } else {
+        console.log(`Got ${issue}!`);
+        findChildren(issue);
+      }
+  });
+}
+
+var findChildren = function(issuekey) {
+  console.log(`Searching for children of ${issuekey}...`);
+  jira.issue.search({
+    'jql': 'cf[11100]=${issuekey}'
+  },
+  function(error, childIssues) {
+    if (error) {
+      console.log(error);
+      return;
+    } else {
+      console.log(`Got ${childIssues}!`);
+    }
+  });
+}
+
+findChildren(issuekey);
+
+
+/*
+fs.readFile('./portfolio_test.csv', function (err, fileData) {
+  parse(fileData, {columns: true, trim: true}, function(err, data) {
+    extractColumns(data);
+  });
+});
+*/
