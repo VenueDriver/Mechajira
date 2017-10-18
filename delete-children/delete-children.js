f6825702e9cc11f473c20954b51aca7c7ee7403d
David Collado <dmcollado@gmail.com>
2017-10-18 12:34:33 -0400

    same

--- delete-children/delete-children.js
+++ delete-children/delete-children.js
@@ -31,8 +31,8 @@ var getIssue = function(issuekey) {
 
 var findChildren = function(issuekey) {
   console.log(`Searching for children of ${issuekey}...`);
-  jira.search({
-    'jql': 'cf[11100]=${issuekey}'
+  jira.search.search({
+    'jql': "cf[11100]=" + `${issuekey}`
   },
   function(error, childIssues) {
     if (error) {
