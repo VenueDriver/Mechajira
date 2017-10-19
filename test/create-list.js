71a690182eba8d3942f6dcafd81a3ca55c9e5b8b
Ryan Porter <rap@endymion.com>
2017-10-18 22:39:49 -0400

    Now create-list can take a list of issue hashes.

--- test/create-list.js
+++ test/create-list.js
@@ -8,13 +8,13 @@ describe('CreateList', function() {
 
   describe('#process()', function() {
 
-    it('should make repeated calls to jira.createIssue.',
+    it('should create JIRA issues from a YAML list of summaries.',
     async function() {
       var getClientStub = sinon.stub(Jira.prototype, "getClient");
       var createIssueStub = sinon.stub(Jira.prototype, "createIssue").
         resolves({issueKey: 'ABC-123'})
       var createList = new CreateList({silent: true})
-      await createList.process('test/files/create-list.yml')
+      await createList.process('test/files/create-list-from-summaries.yml')
       sinon.assert.callCount(createIssueStub, 4)
       sinon.assert.calledWith(createIssueStub,
         {
@@ -46,6 +46,50 @@ describe('CreateList', function() {
           issuetype: "Task",
           summary: "Do something about gamma",
           description: "This is a task about gamma",
+          epic: "MECTEST-1"
+        })
+      getClientStub.restore()
+      createIssueStub.restore()
+    })
+
+    it('should create JIRA issues from a YAML list of issue hashes.',
+    async function() {
+      var getClientStub = sinon.stub(Jira.prototype, "getClient");
+      var createIssueStub = sinon.stub(Jira.prototype, "createIssue").
+        resolves({issueKey: 'ABC-123'})
+      var createList = new CreateList({silent: true})
+      await createList.process('test/files/create-list-from-issues.yml')
+      sinon.assert.callCount(createIssueStub, 4)
+      sinon.assert.calledWith(createIssueStub,
+        {
+          project: "MECTEST",
+          issuetype: "Task",
+          summary: "Do something about alpha",
+          description: "This is a task about ALPHA",
+          epic: "MECTEST-1"
+        })
+      sinon.assert.calledWith(createIssueStub,
+        {
+          project: "MECTEST",
+          issuetype: "Task",
+          summary: "Do something about beta",
+          description: "This is a task about BETA",
+          epic: "MECTEST-1"
+        })
+      sinon.assert.calledWith(createIssueStub,
+        {
+          project: "MECTEST",
+          issuetype: "Task",
+          summary: "Do something about delta",
+          description: "This is a task about DELTA",
+          epic: "MECTEST-1"
+        })
+      sinon.assert.calledWith(createIssueStub,
+        {
+          project: "MECTEST",
+          issuetype: "Task",
+          summary: "Do something about gamma",
+          description: "This is a task about GAMMA",
           epic: "MECTEST-1"
         })
       getClientStub.restore()
