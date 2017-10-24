03553d515193a43034ff3620e0e072e58fdc5ba4
David Collado <dmcollado@gmail.com>
2017-10-24 16:52:17 -0400

    adding /lib/edit-children.js /lib/find-children.js and mechajira-edit-children.js

--- mechajira.js
+++ mechajira.js
@@ -7,4 +7,6 @@ program
   .command('create-list [options] <file>', 'Create a list of JIRA issues from a YAML command file.').alias('cl')
   .command('portfolio [options] <file>', 'Update JIRA issues from a JIRA Portfolio export CSV file.').alias('p')
   .command('read-issue [options] <key>', 'Print the data for a given issue in JSON form.').alias('r')
+  .command('find-children [options] <parent>', 'Find all children of a parent issue.').alias('fc')
+  .command('edit-children [options] <file> <parent>', 'Edit all children of a parent with fields set in a YAML file.').alias('ec')
   .parse(process.argv);
