61ae08c0436a565d47f5428624e3c274740ec7c3
David Collado <dmcollado@gmail.com>
2017-11-08 11:57:51 -0500

    Got mechajira-find-children working just spitting out child issues.... next is to parlay that into mechajira-edit-children and edit those issues based on props/vals in a commandfile

---
+++ mechajira-find-children.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node --harmony
+var program = require('commander');
+var chalk = require('chalk');
+
+// Returns a hash of configuration values.
+const configData = require('./lib/config').configData;
+// Performs the API call.
+const Jira = require('./lib/jira');
+
+program
+  .description('Print the child issues under a given parent issue.')
+  .arguments('<key>')
+  .usage('[options] r <key>')
+  .option('-h, --host <host>', "The host name of the JIRA instance.")
+  .option('--port <port>', "The port for the JIRA instance connection.")
+  .option('-u, --username <username>', "The JIRA user's username.")
+  .option('-p, --password <password>', "The JIRA user's password.")
+  .option('-v, --verbose', "Log lots of extra details.")
+  .option('-s, --silent', "Don't log anything unless there is an error.")
+  .action(async function (key) {
+    try {
+      var jira = new Jira(configData(program))
+      var issue = await jira.findChildren(key);
+      console.dir(issue);
+    } catch (e) {
+      console.log(chalk.bold.red(e));
+    }
+  })
+  .parse(process.argv);
