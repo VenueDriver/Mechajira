844eabce7e47e8a40b518bfbb810522f46ba606b
David Collado <dmcollado@gmail.com>
2017-11-22 11:04:20 -0500

    deleteIssue works for the most part, but it won't delete issues with subtasks because I can't seem to properly set the deleteSubtasks parameter in jira.deleteIssue

--- mechajira-bulk-op.js
+++ mechajira-bulk-op.js
@@ -23,7 +23,7 @@ program
   .option('-c, --close', "Close all descendents under the ancestor (including the ancestor).")
   .option('-t, --trans <transID>', "Transition all descendents under the ancestor (including the ancestor).")
   .option('-gt, --gettrans', "Get a list of transitions available for a given issue.")
-  .option('-e, --edit <fields...>', "Edit all descendents under the ancestor with the following fields/values.")
+  .option('-e, --edit [options...]', "Edit all descendents under the ancestor with the following fields/values.")
   .option('-a, --assignee <assignee>', "New assignee for tasks.")
   .option('-dd, --duedate <duedate>', "New duedate for tasks.")
   .action(function (key, op, options) {
